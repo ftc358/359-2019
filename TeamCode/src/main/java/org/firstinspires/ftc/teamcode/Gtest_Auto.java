3cace342e4d1d238f24d72f01c4b6827e3b53eff
garbochung <g13what.never@gmail.com>
2019-10-25 15:33:13 -0700

    359 Update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gtest_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gtest_Auto.java
@@ -2,17 +2,17 @@
 
 package org.firstinspires.ftc.teamcode;
 
-        import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-        import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-        import com.qualcomm.robotcore.hardware.DcMotor;
-        import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-        import org.firstinspires.ftc.robotcore.external.ClassFactory;
-        import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
-        import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
-        import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 
-        import java.util.List;
+import java.util.List;
 
 
 @Autonomous
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
@@ -4,13 +4,14 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
 public class TeleOp359 extends LinearOpMode {
 
     DcMotor motor1, motor2, motor3, motor4;
-//        DcMotor motor1i, motor2i;
-//        Servo servoNeck, servoGrip;
+    DcMotor motor1i, motor2i;
+    // Servo servoNeck, servoGrip;
 
     public void runOpMode() throws InterruptedException {
 
@@ -23,6 +24,9 @@ public class TeleOp359 extends LinearOpMode {
         motor4 = hardwareMap.dcMotor.get("motor4");
         motor4.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
+        motor1i = hardwareMap.dcMotor.get("motor1i");
+        motor2i = hardwareMap.dcMotor.get("motor2i");
+
 
         waitForStart();
         while (opModeIsActive()) {
@@ -32,22 +36,64 @@ public class TeleOp359 extends LinearOpMode {
 
             // mecanum wheels
             double leftX = gamepad1.left_stick_x;
+            double rightY= -gamepad1.right_stick_y;
+            double rightX= gamepad1.right_stick_x;
 
-            final double v1 = gamepad1.right_stick_y - gamepad1.right_stick_x - leftX;
-            final double v2 = gamepad1.right_stick_y + gamepad1.right_stick_x + leftX;
-            final double v3 = gamepad1.right_stick_y - gamepad1.right_stick_x + leftX;
-            final double v4 = gamepad1.right_stick_y + gamepad1.right_stick_x - leftX;
+            final double v1 = rightY - rightX - leftX;
+            final double v2 = rightY + rightX + leftX;
+            final double v3 = rightY - rightX + leftX;
+            final double v4 = rightY + rightX - leftX;
 
-            motor1.setPower(-0.5 * v1);
-            motor2.setPower(0.5 * v2);
-            motor3.setPower(0.5 * v3);
-            motor4.setPower(-0.5 * v4);
+            motor1.setPower(0.7 * v1);
+            motor2.setPower(-0.7 * v2);
+            motor3.setPower(-0.7 * v3);
+            motor4.setPower(0.7 * v4);
 
-            telemetry.addData("motor1", v1);
-            telemetry.addData("motor2", v2);
-            telemetry.addData("motor3", v3);
-            telemetry.addData("motor4", v4);
+            telemetry.addData("motor1", motor1.getPower());
+            telemetry.addData("motor2", motor2.getPower());
+            telemetry.addData("motor3", motor3.getPower());
+            telemetry.addData("motor4", motor4.getPower());
             telemetry.update();
+
+            // intake mechanism
+            boolean leftBump = gamepad1.left_bumper;
+            boolean rightBump = gamepad1.right_bumper;
+
+            motor1i.setPower(0);
+            motor2i.setPower(0);
+
+            if (leftBump) {
+                motor1i.setPower(-1);
+                motor2i.setPower(1);
+            }
+            else if (rightBump) {
+                motor1i.setPower(1);
+                motor2i.setPower(-1);
+            }
+
+
+//            // Placement mechanism -- neck
+//            float neckleft=gamepad1.left_trigger;
+//            float neckright=gamepad1.right_trigger;
+//
+//            if (neckleft>0.1) {
+//                servoNeck.setPosition(servoNeck.getPosition()-0.1);
+//            }
+//            if (neckright>0.1) {
+//                servoNeck.setPosition(servoNeck.getPosition()+0.1);
+//            }
+//
+//
+//            // Placement mechanism -- Grip
+//            boolean a =gamepad1.a;
+//            boolean up=true;
+//
+//            if (a && up) {
+//                servoGrip.setPosition(0.25);
+//            }
+//            else if (a && !up) {
+//                servoGrip.setPosition(0);
+//            }
 
         }
     }
