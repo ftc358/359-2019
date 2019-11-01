f739d0572db68065a369ac97cb2f10e3999001a5
Charlie Sun <csun@webb.org>
2019-11-01 00:02:52 -0700

    Charlie update: class Encoders359 and a guess code autonomous for meet 0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp359.java
@@ -2,19 +2,20 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
-public class TeleOp359 extends LinearOpMode {
+public class TeleOp359 extends OpMode {
 
     DcMotor motor1, motor2, motor3, motor4;
     DcMotor motor1i, motor2i;
 //    Servo servoNeck, servoGrip;
 
-    public void runOpMode() throws InterruptedException {
+    public void init() {
 
         motor1 = hardwareMap.dcMotor.get("motor1");
         motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -33,51 +34,47 @@ public class TeleOp359 extends LinearOpMode {
 
 //        servoNeck = hardwareMap.servo.get("servoN");
 //        servoGrip = hardwareMap.servo.get("servoG");
+    }
 
+    public void loop() {
 
-        waitForStart();
-        while (opModeIsActive()) {
-            telemetry.addData("opModeIsActive", opModeIsActive());
-            telemetry.update();
+        // mecanum wheels
+        double leftX = gamepad1.left_stick_x;
+        double rightY= -gamepad1.right_stick_y;
+        double rightX= gamepad1.right_stick_x;
 
+        final double v1 = rightY - rightX - leftX;
+        final double v2 = rightY + rightX + leftX;
+        final double v3 = rightY - rightX + leftX;
+        final double v4 = rightY + rightX - leftX;
 
-            // mecanum wheels
-            double leftX = gamepad1.left_stick_x;
-            double rightY= -gamepad1.right_stick_y;
-            double rightX= gamepad1.right_stick_x;
+        motor1.setPower(0.7 * v1);
+        motor2.setPower(0.7 * v2);
+        motor3.setPower(0.7 * v3);
+        motor4.setPower(0.7 * v4);
 
-            final double v1 = rightY - rightX - leftX;
-            final double v2 = rightY + rightX + leftX;
-            final double v3 = rightY - rightX + leftX;
-            final double v4 = rightY + rightX - leftX;
+        telemetry.addData("motor1", motor1.getPower());
+        telemetry.addData("motor2", motor2.getPower());
+        telemetry.addData("motor3", motor3.getPower());
+        telemetry.addData("motor4", motor4.getPower());
+        telemetry.update();
 
-            motor1.setPower(0.7 * v1);
-            motor2.setPower(0.7 * v2);
-            motor3.setPower(0.7 * v3);
-            motor4.setPower(0.7 * v4);
+        // intake mechanism
+        boolean leftBump = gamepad1.left_bumper;
+        boolean rightBump = gamepad1.right_bumper;
 
-            telemetry.addData("motor1", motor1.getPower());
-            telemetry.addData("motor2", motor2.getPower());
-            telemetry.addData("motor3", motor3.getPower());
-            telemetry.addData("motor4", motor4.getPower());
-            telemetry.update();
-
-            // intake mechanism
-            boolean leftBump = gamepad1.left_bumper;
-            boolean rightBump = gamepad1.right_bumper;
-
-            if (leftBump == true) {
-                motor1i.setPower(-1);
-                motor2i.setPower(1);
-            }
-            else if (rightBump == true) {
-                motor1i.setPower(1);
-                motor2i.setPower(-1);
-            }
-            else{
-                motor1i.setPower(0);
-                motor2i.setPower(0);
-            }
+        if (leftBump == true) {
+            motor1i.setPower(-1);
+            motor2i.setPower(1);
+        }
+        else if (rightBump == true) {
+            motor1i.setPower(1);
+            motor2i.setPower(-1);
+        }
+        else{
+            motor1i.setPower(0);
+            motor2i.setPower(0);
+        }
 
 
 //            // Placement mechanism -- neck
@@ -103,7 +100,5 @@ public class TeleOp359 extends LinearOpMode {
 //                servoGrip.setPosition(0);
 //            }
 
-        }
     }
-
 }
